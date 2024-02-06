be41150cfcae32e8483c1af0fd3e3a7787b4b4f3
Brian Schott <bschott@cgps.org>
2024-02-06 12:30:06 -0500

    Tuesday's work

---
+++ Solutions.java
@@ -0,0 +1,15 @@
+public class Solutions {
+    public static void main(String[] args) {
+
+    }
+    public static int[][] scale(int[][] input, int scalingFactor) {
+        //create a new 2d array called output w/ row/col count the same
+
+        //iterate through the 2d array input
+        //for each value, multiply it by the scaling factor
+        //place that value in the output array in the same spot
+
+        //return new array output
+    }
+
+}
